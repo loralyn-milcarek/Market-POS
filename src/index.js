237afe1dc8c8d577ebdfed2201394232accab3b0
loralyn-milcarek <loralyn.milcarek@gmail.com>
2022-05-03 19:37:42 -0400

    webpack and react

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import style from '../styles/style.css';
+
+ReactDOM.render(
+    <h1>nice work</h1>,
+    document.getElementById('root')
+);
